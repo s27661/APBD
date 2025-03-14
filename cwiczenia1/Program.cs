860d4be13f65c58bcdacda0acc1d30b938731af3
unknown <s27661@pjwstk.edu.pl>
2025-03-14 19:44:40 +0100

    Modyfikacja 1

--- cwiczenia1/Program.cs
+++ cwiczenia1/Program.cs
@@ -5,6 +5,20 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            for (var i = 0; i < 10; i++)
+            {
+                for (var j = 0; j < 10; j++)
+                {
+                    if (j == 9)
+                    {
+                        Console.WriteLine("*");
+                    }
+                    else
+                    {
+                        Console.Write("* ");
+                    }
+                }
+            }
         }
     }
 }
