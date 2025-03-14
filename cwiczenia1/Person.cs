b676de7d40ce2da4b614914a66bdeeb5b4326caf
unknown <s27661@pjwstk.edu.pl>
2025-03-14 19:50:25 +0100

    Modyfikacja 2

---
+++ cwiczenia1/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cwiczenia1
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public Person(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+    }
+
+    
+}
